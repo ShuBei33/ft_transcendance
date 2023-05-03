8353713d0ced5acf138c63eb254b395e1afaca33
mychibani <m.yassinechibani@gmail.com>
2023-05-03 17:55:19 +0200

    feat: Initialisation du Back (pour le projet), debut de setup de prisma et connection avec le docker compose avec la db

---
+++ srcs/backend/src/app.controller.ts
@@ -0,0 +1,16 @@
+/*
+**		Controlleur basique avec une seule route, qui va return 'Hello World'
+*/
+
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
---
+++ srcs/backend/src/app.module.ts
@@ -0,0 +1,14 @@
+/*
+**			Module principal de l'application
+*/
+
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
