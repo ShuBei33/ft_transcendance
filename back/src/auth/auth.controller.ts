aa9914c667e741dc7e72dd0bed657e79fcdf3887
Maxime LORMOIS <mlormois@paul-f4Ar5s8.clusters.42paris.fr>
2023-08-09 01:27:15 +0200

    :package: Installation et Settings de JWT :package:

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -1,23 +1,22 @@
 import { Controller, Get, HttpCode, HttpStatus, Post, Req, Res, UseGuards } from '@nestjs/common';
-import { FtGuard } from './guard';
+import { FtGuard, JwtGuard } from './guard';
 import { Request, Response } from 'express';
 import { AuthService } from './auth.service';
 import { FTUser } from './dto';
+import { GetToken, GetUser } from './decorator';
 
 @Controller('auth')
 export class AuthController {
 	constructor(private authService: AuthService) {}
 
-	// FONCTION TEST POUR SAVOIR SI LA CONNECTION RESEAU EST OPERATIONNELLE
-	// 1: Lancez vos containers
-	// 2: depuis votre machine: curl ${API}/auth/me   res = u get me bro
-	// 3: depuis une autre machine: curl ${API}/auth/me   res = u get me bro
-
-	// ATTENTION vous devez avoir mis votre IP dans ${BACK_HOST}
-	// pour la recuperer MACOS: ipconfig getifaddr en1 
-
-	@Get('me')
-	async getMe() { return ( "u get me bro." ); }
+	@UseGuards(JwtGuard)
+	@HttpCode(HttpStatus.OK)
+	@Get('checkJWT')
+	async checkJWT(@GetUser() user, @GetToken() tokens) {
+		console.log("user: ", user);
+		console.log("tokens: ", tokens);
+		return ({ user: user });
+	}
 
 	@UseGuards(FtGuard)
 	@HttpCode(HttpStatus.OK)
@@ -28,8 +27,7 @@ export class AuthController {
 	@Get('callback')
 	async callback(@Req() req: Request) {
 		const data = req.user as FTUser;
-		await this.authService.saveToken( data.access_token, data.refresh_token);
-		await this.authService.signin42( data.username );
+		return this.authService.signin42( data.username, data.access_token, data.refresh_token );
 	}
 
 }
