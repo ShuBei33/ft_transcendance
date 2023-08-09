90b451228367f15cac1d86c2104773cf46d63ff9
Maxime LORMOIS <mlormois@paul-f4Ar6s7.clusters.42paris.fr>
2023-08-09 23:12:22 +0200

    ✨ Init de Swagger ✨

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder()
+		.setTitle('Ft_transendence')
+		.setDescription('42 project API')
+		.setVersion('1.0')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+
 	app.useGlobalPipes(new ValidationPipe);
 	await app.listen(process.env.BACK_PORT);
 }
