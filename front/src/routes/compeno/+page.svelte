52e6cc9501bd1a1260f049bfc2fe69eb57f9c6a7
Eclipse TOURE <motoure@student.42.fr>
2023-08-12 00:20:40 +0200

    save

---
+++ front/src/routes/compeno/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+</script>
+
+<main>
+
+    <!-- <button -->
+        <!-- class={'buttonClass'}>PLAY</button> -->
+    
+        <div class="square"></div>
+    </main>
+
+<style lang="scss">
+    
+    .square {
+        height: 100px;
+        width: 100px;
+        border: 2px solid salmon;
+        &:hover {
+            height:
+        }
+    }
+    // .buttonClass {
+    //      height: 30px;
+    //      width: 90px;
+    //      font-family: Helvetica;
+    //      font-
+    //      border-radius: 10px;
+    //      background: rgb(198, 198, 238);
+    //      border: 2px solid #0336c4;
+    // }
+</style>
