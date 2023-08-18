2ddf1d9acabc2c420260bd69d960cec9c72aa426
Eclipse TOURE <motoure@student.42.fr>
2023-08-18 22:04:19 +0200

    🚑 fonction generation pseudo fonctionne pas encore, login utilisee pour pseudo for now

--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -2,53 +2,73 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LIST_SUFFIX_PSEUDO } from './data';
+import { Logger } from '@nestjs/common';
+const logger = new Logger();
 
 @Injectable()
 export class AuthService {
-	constructor(
-		private jwt: JwtService,
-		private prisma: PrismaService,
-	) {}
+  constructor(private jwt: JwtService, private prisma: PrismaService) {}
 
-	async user_validator( login: string ) {
-		const ret = await this.prisma.user.count({ where: { login: login } });
-		return ret != undefined ? true : false;
-	}
+  async user_validator(login: string) {
+    const ret = await this.prisma.user.count({ where: { login: login } });
+    return ret != 0;
+  }
 
-	async pseudo_validator( pseudo: string ) {
-		const ret = await this.prisma.user.count({ where: { pseudo: pseudo } });
-		return ret != undefined ? true : false;
-	}
+  async pseudo_validator(pseudo: string) {
+    const ret = await this.prisma.user.count({ where: { pseudo: pseudo } });
+    return ret != undefined ? true : false;
+  }
 
-	async pseudo_create( login: string ): Promise<string> {
-		let pseudo = login + LIST_SUFFIX_PSEUDO[Math.floor(Math.random() * LIST_SUFFIX_PSEUDO.length)];
-		while ( await this.pseudo_validator( pseudo ) )
-			pseudo = login + LIST_SUFFIX_PSEUDO[Math.floor(Math.random() * LIST_SUFFIX_PSEUDO.length)];
-		return pseudo;
-	}
+  async pseudo_create(login: string): Promise<string> {
+    let pseudo =
+      login +
+      LIST_SUFFIX_PSEUDO[Math.floor(Math.random() * LIST_SUFFIX_PSEUDO.length)];
+    while (await this.pseudo_validator(pseudo))
+      pseudo =
+        login +
+        LIST_SUFFIX_PSEUDO[
+          Math.floor(Math.random() * LIST_SUFFIX_PSEUDO.length)
+        ];
+    return pseudo;
+  }
 
-	async signup( login: string ) {
-		const user = await this.prisma.user.create({
-			data: {
-				login: login,
-				pseudo: await this.pseudo_create( login ),
-				rank: 1800,
-			}
-		});
-		const { createdAt, updateAt, twoFA, ...rest } = user;
-		return rest;
-	}
+  async signup(login: string) {
+    const user = await this.prisma.user.create({
+      data: {
+        login,
+        pseudo: login,
+        rank: 1,
+      },
+    });
+    const { createdAt, updateAt, twoFA, ...rest } = user;
+    return rest;
+  }
 
-	async signToken(refresh_token: string, access_token: string, login: string): Promise<{ token: string }> {
-		const payload = { refresh: refresh_token, access: access_token, login: login }
-		const secret = process.env.JWT_SECRET;
-		const token = await this.jwt.signAsync(payload, { expiresIn: '1h', secret: secret });
-		return { token: token };
-	}
-	
-	async signin42( login: string, access_token: string, refresh_token: string ): Promise<{ token: string }> {
-		if ( await this.user_validator(login) == false )
-			this.signup( login );
-		return await this.signToken( refresh_token, access_token, login) ;
-	}
+  async signToken(
+    refresh_token: string,
+    access_token: string,
+    login: string,
+  ): Promise<{ token: string }> {
+    const payload = {
+      refresh: refresh_token,
+      access: access_token,
+      login: login,
+    };
+    const secret = process.env.JWT_SECRET;
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '1h',
+      secret: secret,
+    });
+    return { token: token };
+  }
+
+  async signin42(
+    login: string,
+    access_token: string,
+    refresh_token: string,
+  ): Promise<{ token: string }> {
+    logger.log('singin42()');
+    if ((await this.user_validator(login)) == false) this.signup(login);
+    return await this.signToken(refresh_token, access_token, login);
+  }
 }
