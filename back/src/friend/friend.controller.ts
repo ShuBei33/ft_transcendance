b9d6c44fea3821ee153051d3d4a07c5011e1949b
nattensleplubo <nathan.gobert2002@gmail.com>
2023-08-23 17:14:45 +0200

    :sparkles: Delete and Show friends list should be working !

--- back/src/friend/friend.controller.ts
+++ back/src/friend/friend.controller.ts
@@ -11,7 +11,12 @@ import { DTO_AnswerInvitation, DTO_AnswerInvitationSchema } from './dto';
 //     - addFriend // SEULEMENT EN SERVICE
 // }
 
-@UseGuards(JwtGuard)
+const user1 = {
+	id: 1,
+};
+
+
+// @UseGuards(JwtGuard)
 @ApiTags('Friend')
 @ApiBearerAuth()
 @Controller('friend')
@@ -23,19 +28,19 @@ export class FriendController {
     @ApiResponse({ status: 200, description: 'Succes de la Requete' })
     @ApiResponse({ status: 400, description: 'Echec de la Requete' })
 	async get(
-		@GetUser() user: User,
+		// @GetUser() user: User, //! A DECOMMENTER
 		@Res() res: Response 
 	) { try {
 			console.log('FUNCTION Get Friend was called');
-			console.log('JWT User: ', user);
+			// console.log('JWT User: ', user); //! A DECOMMENTER
 
 			// CODE ICI
-			// const data = service. 
+			const data = await this.friendService.getFriendsList(user1.id);
 			
 			return res.status(200).json({
 				success: true,
 				message: "La recuperation de la liste d'amis c'est bien passe",
-				// data: data,
+				data: data,
 			});
 		} catch (err: any) {
 			return res.status(400).json({
@@ -52,14 +57,15 @@ export class FriendController {
 	@ApiParam({ name: 'uid', description: 'ID de l\'user cible', type: 'number', example: 1 })
 	async remove(
 		@Param('uid', ParseIntPipe) uid: number, 
-		@GetUser() user: User,
+		// @GetUser() user: User,
 		@Res() res: Response,
 	) { try {
 			console.log('FUNCTION Remove Friend was called');
-			console.log('JWT User: ', user);
+			// console.log('JWT User: ', user);
 			console.log('User Id Cible: ', uid);
 
-			// CODE ICI
+			await this.friendService.deleteFriend(user1.id, uid);
+			await this.friendService.deleteFriend(uid, user1.id);
 
             return res.status(200).json({success: true });
 		} catch (err: any) {
--- back/src/friend/friend.service.ts
+++ back/src/friend/friend.service.ts
@@ -1,4 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class FriendService {}
+export class FriendService {
+  constructor(private prisma: PrismaService) {}
+
+  async getFriendsList(userId: number) : Promise<User[]> {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+      include: {
+        sentInvites: {
+          where: { inviteStatus: 'ACCEPTED' }, // Filter for accepted friend requests
+          include: { receiver: true },
+        },
+        receivedInvites: {
+          where: { inviteStatus: 'ACCEPTED' }, // Filter for accepted friend requests
+          include: { sender: true },
+        },
+      },
+    });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    // Combine sent and received friend requests into a single array
+    const friends = [
+      ...user.sentInvites.map((invite) => invite.receiver),
+    //   ...user.receivedInvites.map((invite) => invite.sender),
+    ];
+
+    return friends;
+  }
+
+  async deleteFriend(userId: number, friendId: number) {
+    // Check if the users are friends
+    const friendship = await this.prisma.friendship.findFirst({
+      where: {
+        OR: [
+          { senderId: userId, receiverId: friendId },
+          { senderId: friendId, receiverId: userId },
+        ],
+        inviteStatus: 'ACCEPTED',
+      },
+    });
+
+    if (!friendship) {
+      throw new NotFoundException('Friendship not found');
+    }
+
+    // Delete the friendship
+    await this.prisma.friendship.delete({
+      where: { id: friendship.id },
+    });
+
+    return 'Friend deleted successfully';
+  }
+}
