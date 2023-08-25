c964224803e2cd9814086c2008d8198c54d738ef
Maxime LORMOIS <mlormois@paul-f4Ar6s5.clusters.42paris.fr>
2023-08-25 17:27:28 +0200

    save chec front

--- back/src/app.gateway.ts
+++ back/src/app.gateway.ts
@@ -5,25 +5,27 @@ import { Server, Socket } from 'socket.io';
 import { FTAuth } from './auth/dto';
 import { UserLite } from './user/dto';
 import { UserService } from './user/user.service';
-import { UserStatus } from '@prisma/client';
+import { User, UserStatus } from '@prisma/client';
 
 @WebSocketGateway({ cors: '*:*', })
 export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 
-	constructor( private jwt: JwtService, private user: UserService ) {}
+	constructor( private jwt: JwtService, private userService: UserService ) {}
 
 	private clientsMap = new Map<UserLite, Socket>();
 	@WebSocketServer() wss: Server;
 
 	private logger: Logger = new Logger('AppGateway');
 
+	////////////////////////////////////
+	//  INIT, CONNECTION, DISCONNECT  //
+	////////////////////////////////////
+
 	afterInit(server: Server) {
 		this.logger.log('AppGateway Initialized');
 	}
 
 	handleConnection(client: Socket, ...args: any[]) {
-
-		// AUTH
 		let user: UserLite;
 		const token = client.handshake.auth.token;
 		try {
@@ -32,27 +34,84 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
 			client.disconnect();
 		}
 
-		//SAVE_AUTH
-		if (this.clientsMap.has(user)) {
+		if ( this.isConnected( user.id )) {
 			this.logger.error(`USER ${user.login} ALREADY LOGGED IN appGateway`);
-			throw new WsException(`double connection`);
+			client.disconnect();
 		} else {
-			this.logger.log(`CLIENT [${user.id}]${user.login} CONNECTED`);
+			this.logger.log(`USER ${user.login} [${user.id}] CONNECTED`);
 			this.clientsMap.set(user, client);
-			// FUNC USER 
-
-
-			client.broadcast.emit('loginToClient', { userId: user.id, status: UserStatus.ONLINE });
+			this.userService.updateUserStatus(user.id, UserStatus.ONLINE);
+			this.wss.emit('loginToClient', user.id);
 		}
 	}
 
 	handleDisconnect(client: Socket) {
-		this.logger.log(`CLIENT ${client.id} DISCONNECTED`);
+		for (const [user, value] of this.clientsMap) {
+			if (client.id === value.id) {
+				this.logger.log(`USER ${user.login} [${user.id}] DISCONNECTED`);
+				this.userService.updateUserStatus(user.id, UserStatus.OFFLINE);
+				this.wss.emit('logoutToClient', user.id);
+				this.clientsMap.delete(user);
+				break;
+			}
+		}
 	}
 
-	@SubscribeMessage('message')
-	handleMessage(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
-	  this.wss.emit('message', client.id, data);
+	///////////////////
+	// TOOLS METHODS //
+	///////////////////
+
+	isConnected( uid: number ) {
+		for (const [key, value] of this.clientsMap) {
+			if ( key.id == uid )
+				return true;
+		}
+		return false
 	}
+
+	getUser( sid: string ) {
+		for (const [key, value] of this.clientsMap) {
+			if ( value.id == sid )
+				return key;
+		}
+	}
+
+	dispayClientsMap() {
+		this.logger.log('=== number of clients = ' + this.wss.engine.clientsCount)
+		for (const [key, value] of this.clientsMap) {
+			this.logger.log(`\t User ${key.login} [${key.id}] =\t${value.id}`);
+		}
+	}
+
+	//////////////
+	//  EVENTS  //
+	//////////////
 	
+	@SubscribeMessage('userStatus')
+	handleStatus(@MessageBody() newStatus: UserStatus, @ConnectedSocket() client: Socket) {
+		const userId: number = this.getUser( client.id ).id;
+		this.userService.updateUserStatus(userId, newStatus);
+		this.wss.emit('updateStatus', { id: userId, status: newStatus});
+	}
+
+
+
+
+
+
+
+    ////////////////////
+    //  CHAT METHODS  //
+    ////////////////////
+
+	joinAllDiscussion( socket: Socket, user: UserLite) {
+
+	}
+
+	joinAllChannel( socket: Socket, user: UserLite) {
+		// getMyChannels
+	}
+
+
+
 }
