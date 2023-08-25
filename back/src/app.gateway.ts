5ce0155969a2392c5beda5dc412732506eb1de63
Maxime LORMOIS <mlormois@paul-f4Ar6s5.clusters.42paris.fr>
2023-08-25 16:00:28 +0200

    Push de Socket INIT status

--- back/src/app.gateway.ts
+++ back/src/app.gateway.ts
@@ -1,11 +1,18 @@
 import { Logger } from '@nestjs/common';
-import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { JwtService } from '@nestjs/jwt';
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer, WsException } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { FTAuth } from './auth/dto';
+import { UserLite } from './user/dto';
+import { UserService } from './user/user.service';
+import { UserStatus } from '@prisma/client';
 
 @WebSocketGateway({ cors: '*:*', })
 export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 
-	private clientsMap = new Map<number, Socket>(); // userid / socket associer au user
+	constructor( private jwt: JwtService, private user: UserService ) {}
+
+	private clientsMap = new Map<UserLite, Socket>();
 	@WebSocketServer() wss: Server;
 
 	private logger: Logger = new Logger('AppGateway');
@@ -15,15 +22,33 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
 	}
 
 	handleConnection(client: Socket, ...args: any[]) {
-		this.logger.log(`CLIENT ${client.id} CONNECTED: `);
-		console.log(client.handshake.auth);
+
+		// AUTH
+		let user: UserLite;
+		const token = client.handshake.auth.token;
+		try {
+			user = (this.jwt.verify(token, { secret: process.env.JWT_SECRET}).user);
+		} catch (e) {
+			client.disconnect();
+		}
+
+		//SAVE_AUTH
+		if (this.clientsMap.has(user)) {
+			this.logger.error(`USER ${user.login} ALREADY LOGGED IN appGateway`);
+			throw new WsException(`double connection`);
+		} else {
+			this.logger.log(`CLIENT [${user.id}]${user.login} CONNECTED`);
+			this.clientsMap.set(user, client);
+			// FUNC USER 
+
+
+			client.broadcast.emit('loginToClient', { userId: user.id, status: UserStatus.ONLINE });
+		}
 	}
 
 	handleDisconnect(client: Socket) {
 		this.logger.log(`CLIENT ${client.id} DISCONNECTED`);
 	}
-
-
 
 	@SubscribeMessage('message')
 	handleMessage(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
