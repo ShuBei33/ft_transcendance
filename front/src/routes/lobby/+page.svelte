e890791301b5360c15a4ac08f72f53b595218465
Enora STOFFEL <estoffel@student.42.fr>
2023-08-28 02:25:35 +0200

    lvl + money buttons

--- front/src/routes/lobby/+page.svelte
+++ front/src/routes/lobby/+page.svelte
@@ -3,11 +3,16 @@
     let redirectTo = "http://localhost:5500/game/joinQueue";
 </script>
 
+<div class="grid-container"> 
 
-<div class="profile">
-</div>
-<div class="grid-container">
   <div class="header">
+    <div class="profile"></div>
+    <div class="upgrade">
+      <div class="lvl"></div>
+      <div class="money"></div>
+    </div>
+    <div class="minicub-lvl">lv</div>
+    <div class="minicub-money">$</div>
     <span><a href="/lobby">LOBBY</a></span>
     <span><a href="/store">STORE</a></span>
     <span><a href="/leaderboard">LEADERBOARD</a></span>
@@ -47,6 +52,32 @@
   grid-template-rows: 1fr 3fr 1fr;
 }
 
+.upgrade {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  left: 7.5em;
+  row-gap: 0.5em;
+}
+
+.lvl {
+  width: 91px;
+  height: 32px;
+  flex-shrink: 0;
+  border-radius: 9px;
+  border: 2px solid rgba(255, 255, 255, 0.20);
+  background: rgba(255, 255, 255, 0.20);
+}
+
+.money {
+  width: 91px;
+  height: 32px;
+  flex-shrink: 0;
+  border-radius: 9px;
+  border: 2px solid rgba(255, 255, 255, 0.20);
+  background: rgba(255, 255, 255, 0.20);
+}
+
 .header {
   display: flex;
   justify-content: center;
@@ -64,6 +95,50 @@
   position: absolute;
   bottom: 1em;
   right: 1em;
+}
+
+.minicub-lvl {
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  left: 8em;
+  top:0.5em;
+  width: 38px;
+  height: 38px;
+  flex-shrink: 0;
+  border-radius: 9px;
+  border: 2px solid rgba(255, 255, 255, 0.20);
+  background: radial-gradient(95.61% 95.61% at 50.00% 4.39%, rgba(255, 255, 255, 0.30) 0%, rgba(255, 255, 255, 0.00) 100%), linear-gradient(180deg, #00FF1A 0%, #03BB0A 100%);
+  color: #FFF;
+  text-align: center;
+  font-family: Noto Sans Tamil;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+}
+
+.minicub-money {
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  left: 8em;
+  top: 2.4em;
+  width: 38px;
+  height: 38px;
+  flex-shrink: 0;
+  border-radius: 9px;
+  border: 2px solid rgba(255, 255, 255, 0.20);
+  background: radial-gradient(95.61% 95.61% at 50.00% 4.39%, rgba(250, 255, 0, 0.30) 0%, rgba(138, 69, 5, 0.00) 100%), linear-gradient(180deg, #FAFF00 0%, #D98200 100%);
+  color: #FFF;
+  text-align: center;
+  font-family: Noto Sans Tamil;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
 }
 
 a {
@@ -89,6 +164,7 @@ a:hover {
   text-decoration-thickness: 5px;
   text-underline-offset: 8px;
 }
+
 
 
 </style>
