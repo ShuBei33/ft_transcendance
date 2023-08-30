ba8e1b572f8ba6369bf7e8494e3bd27e18590215
Eclipse TOURE <motoure@student.42.fr>
2023-08-30 03:25:15 +0200

    in progress

--- back/src/friend/friend.controller.ts
+++ back/src/friend/friend.controller.ts
@@ -1,130 +1,158 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { FriendService } from './friend.service';
 import { JwtGuard } from 'src/auth/guard';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator';
 import { Response } from 'express';
-import { ApiBearerAuth, ApiHeader, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiHeader,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UserLite } from 'src/user/dto';
+import { success } from 'src/utils/utils_success';
+import { error } from 'src/utils/utils_error';
 
-@UseGuards(JwtGuard) 
+@UseGuards(JwtGuard)
 @ApiBearerAuth()
-@ApiHeader({ name: 'Authorization', description: 'Token d\'authentification' })
+@ApiHeader({ name: 'Authorization', description: "Token d'authentification" })
 @ApiTags('Friend')
 @Controller('friend')
 export class FriendController {
-	constructor(private friendService: FriendService) {}
+  constructor(private friendService: FriendService) {}
 
-	@Get('get')
-    @ApiOperation({ summary: 'Recuperation de la liste d\'amis de l\'utilisateur en court' })
-    @ApiResponse({ status: 200, description: 'Succes de la Requete' })
-    @ApiResponse({ status: 400, description: 'Echec de la Requete' })
-	async get(
-		@GetUser() user: UserLite, 
-		@Res() res: Response 
-	) { try {
-			console.log('FUNCTION Get Friend was called and retrieved friends list for user of id ' + user.id);
-			console.log('JWT User: ', user); 
+  @Get('get')
+  @ApiOperation({
+    summary: "Recuperation de la liste d'amis de l'utilisateur en court",
+  })
+  @ApiResponse({ status: 200, description: 'Succes de la Requete' })
+  @ApiResponse({ status: 400, description: 'Echec de la Requete' })
+  async get(@GetUser() user: UserLite, @Res() res: Response) {
+    try {
+      // CODE ICI
+      const data = await this.friendService.getFriendsList(user.id);
+      return success.general(res, 'Friends retrieved successfully', data);
+    } catch (err: any) {
+      error.unexpected(err);
+    }
+  }
 
-			// CODE ICI
-			const data = await this.friendService.getFriendsList(user.id);
-			
-			return res.status(200).json({
-				success: true,
-				message: "La recuperation de la liste d'amis c'est bien passe",
-				data: data,
-			});
-		} catch (err: any) {
-			return res.status(400).json({
-				success: false,
-				message: err.message,
-			});
-		}
-	}
+  @Delete('remove/:uid')
+  @ApiOperation({
+    summary: "Suppresion d'un utilisateur de votre liste d'amis",
+  })
+  @ApiResponse({ status: 200, description: 'Succes de la suppresion ' })
+  @ApiResponse({ status: 400, description: 'Echec de la suppresion' })
+  @ApiParam({
+    name: 'uid',
+    description: "ID de l'user cible",
+    type: 'number',
+    example: 1,
+  })
+  async remove(
+    @Param('uid', ParseIntPipe) uid: number,
+    @GetUser() user: UserLite,
+    @Res() res: Response,
+  ) {
+    try {
+      console.log('FUNCTION Remove Friend was called');
+      console.log('JWT User: ', user);
+      console.log('User Id Cible: ', uid);
 
-	@Delete('remove/:uid')
-    @ApiOperation({ summary: 'Suppresion d\'un utilisateur de votre liste d\'amis' })
-    @ApiResponse({ status: 200, description: 'Succes de la suppresion ' })
-    @ApiResponse({ status: 400, description: 'Echec de la suppresion' })
-	@ApiParam({ name: 'uid', description: 'ID de l\'user cible', type: 'number', example: 1 })
-	async remove(
-		@Param('uid', ParseIntPipe) uid: number, 
-		@GetUser() user: UserLite, 
-		@Res() res: Response,
-	) { try {
-			console.log('FUNCTION Remove Friend was called');
-			console.log('JWT User: ', user); 
-			console.log('User Id Cible: ', uid);
+      await this.friendService.deleteFriend(user.id, uid); //! Maybe change uid to a string ? idk...
+      await this.friendService.deleteFriend(uid, user.id); //! Cause the user is not going to give an id right ? Or maybe we show id in the front ?
 
-			await this.friendService.deleteFriend(user.id, uid); //! Maybe change uid to a string ? idk...
-			await this.friendService.deleteFriend(uid, user.id); //! Cause the user is not going to give an id right ? Or maybe we show id in the front ?
+      return res.status(200).json({ success: true });
+    } catch (err: any) {
+      return res.status(400).json({ success: false, message: err.message });
+    }
+  }
 
-            return res.status(200).json({success: true });
-		} catch (err: any) {
-            return res.status(400).json({success: false, message: err.message});
+  @Get('getInvitation')
+  @ApiOperation({ summary: 'Recuperation de la liste des invitation Pending' })
+  @ApiResponse({ status: 200, description: 'Succes de la Recuperation' })
+  @ApiResponse({ status: 400, description: 'Echec de la Recuperation' })
+  async getInvitation(@GetUser() user: UserLite, @Res() res: Response) {
+    try {
+      console.log('FUNCTION GetInvitation Friend was called');
+      console.log('JWT User: ', user);
 
-		}
-	}
+      // CODE ICI
+      const data = await this.friendService.getReceivedPendingInvites(user.id);
 
-	@Get('getInvitation')
-    @ApiOperation({ summary: 'Recuperation de la liste des invitation Pending' })
-    @ApiResponse({ status: 200, description: 'Succes de la Recuperation' })
-    @ApiResponse({ status: 400, description: 'Echec de la Recuperation' })
-	async getInvitation(
-		@GetUser() user: UserLite,
-		@Res() res: Response
-	) { try {
-			console.log('FUNCTION GetInvitation Friend was called');
-			console.log('JWT User: ', user);
+      return res.status(200).json({ success: true, data: data });
+    } catch (err: any) {
+      return res.status(400).json({ success: false, message: err.message });
+    }
+  }
 
-			// CODE ICI
-			const data = await this.friendService.getReceivedPendingInvites(user.id);
+  @Post('sendInvitation')
+  @ApiOperation({ summary: "Envoie D'une invitation Ami" })
+  @ApiResponse({
+    status: 200,
+    description: "Succes de l'envoie de l'invitation",
+  })
+  @ApiResponse({
+    status: 400,
+    description: "Echec de l'envoie de l'invitation",
+  })
+  async sendInvitation(
+    @Body('usernameToAdd') usernameToAdd: string,
+    @GetUser() user: UserLite,
+    @Res() res: Response,
+  ) {
+    try {
+      console.log('FUNCTION SendInvitation Friend was called');
+      console.log('JWT User: ', user);
 
-            return res.status(200).json({success: true, data: data });
-		} catch (err: any) {
-            return res.status(400).json({success: false, message: err.message});
-		}	
-	}
+      const response = await this.friendService.sendFriendInvitation(
+        user,
+        'Fantomas',
+      ); // Hamtaro id 3
 
-	@Post('sendInvitation')
-    @ApiOperation({ summary: 'Envoie D\'une invitation Ami' })
-    @ApiResponse({ status: 200, description: 'Succes de l\'envoie de l\'invitation' })
-    @ApiResponse({ status: 400, description: 'Echec de l\'envoie de l\'invitation' })
-	async sendInvitation(
-		@Body('usernameToAdd') usernameToAdd: string,
-		@GetUser() user: UserLite,
-		@Res() res: Response
-	) { try {
-			console.log('FUNCTION SendInvitation Friend was called');
-			console.log('JWT User: ', user);
+      return res.status(200).json({ success: true, response: response });
+    } catch (err: any) {
+      return res.status(400).json({ success: false, message: err.message });
+    }
+  }
 
-			const response = await this.friendService.sendFriendInvitation(user, 'Fantomas'); // Hamtaro id 3
+  @Post('resolveInvitation')
+  @ApiOperation({ summary: "Envoi de la Resolution d'une requete d'ami" })
+  @ApiResponse({ status: 200, description: 'Succes de la Requete' })
+  @ApiResponse({ status: 400, description: 'Echec de la Requete' })
+  async resolveInvitation(
+    @Body() answer: boolean,
+    @Body() fromUser: string,
+    @GetUser() user: UserLite,
+    @Res() res: Response,
+  ) {
+    try {
+      console.log('FUNCTION ResolveInvitation Friend was called');
+      console.log('JWT User: ', user);
+      console.log('Answer Invitation: ', answer);
 
-            return res.status(200).json({success: true, response: response });
-		} catch (err: any) {
-            return res.status(400).json({success: false, message: err.message});
-		}
-	}
+      const response = await this.friendService.acceptFriendInvitation(
+        user.id,
+        answer,
+        fromUser,
+      );
 
-	@Post('resolveInvitation')
-    @ApiOperation({ summary: 'Envoi de la Resolution d\'une requete d\'ami' })
-    @ApiResponse({ status: 200, description: 'Succes de la Requete' })
-    @ApiResponse({ status: 400, description: 'Echec de la Requete' })
-	async resolveInvitation(
-		@Body() answer: boolean,
-		@Body() fromUser: string,
-		@GetUser() user: UserLite,
-		@Res() res: Response
-	) { try {
-			console.log('FUNCTION ResolveInvitation Friend was called');
-			console.log('JWT User: ', user);
-			console.log('Answer Invitation: ', answer);
-
-			const response = await this.friendService.acceptFriendInvitation(user.id, answer, fromUser);
-
-            return res.status(200).json({success: true});
-		} catch (err: any) {
-            return res.status(400).json({success: false, message: err.message});
-		}
-	}
+      return res.status(200).json({ success: true });
+    } catch (err: any) {
+      return res.status(400).json({ success: false, message: err.message });
+    }
+  }
 }
--- front/src/lib/utils/retrieveUserData.svelte
+++ front/src/lib/utils/retrieveUserData.svelte
@@ -8,30 +8,38 @@
   onMount(() => {
     const _Channel = new Channel();
 
-    _Channel.all().then(({ data }) => ($dataStore.channels = data.data));
-    _Channel.mine().then(async ({ data }) => {
-      const myChannels = data.data;
-      const myChannelsIds: number[] = myChannels.map(
-        (chanusr) => chanusr.channel.id
-      );
-      const myChannelsMessages = myChannelsIds.map((chanId) =>
-        _Channel.msgs(chanId)
-      );
+    _Channel
+      .all()
+      .then(({ data }) => ($dataStore.channels = data.data))
+      .catch((e) => {});
+    _Channel
+      .mine()
+      .then(async ({ data }) => {
+        const myChannels = data.data;
+        const myChannelsIds: number[] = myChannels.map(
+          (chanusr) => chanusr.channel.id
+        );
+        const myChannelsMessages = myChannelsIds.map((chanId) =>
+          _Channel.msgs(chanId)
+        );
 
-      // Retrieve every channel messages and append it inside the channels object
-      await Promise.all(myChannelsMessages).then((ChannelMessages) => {
-        ChannelMessages.forEach((message, index) => {
-          const { data } = message;
-          if (data.data.length)
-            myChannels[index].channel.channelMsgs = data.data;
+        // Retrieve every channel messages and append it inside the channels object
+        await Promise.all(myChannelsMessages).then((ChannelMessages) => {
+          ChannelMessages.forEach((message, index) => {
+            const { data } = message;
+            if (data.data.length)
+              myChannels[index].channel.channelMsgs = data.data;
+          });
         });
-      });
-      $dataStore.myChannels = myChannels;
-    });
+        $dataStore.myChannels = myChannels;
+      })
+      .catch((e) => {});
 
-    new Friend().getFriends().then((reponse) => {
-      console.log("!friend data", reponse);
-      //   $dataStore.friends = data.data;
-    });
+    new Friend()
+      .getFriends()
+      .then(({ data }) => {
+        $dataStore.friends = data.data;
+      })
+      .catch((e) => {});
   });
 </script>
