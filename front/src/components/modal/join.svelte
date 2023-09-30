5c9f6a41bd823153edd463de91a7dce5a71c64c6
Eclipse TOURE <motoure@student.42.fr>
2023-09-30 19:04:58 +0200

    updated front

--- front/src/components/modal/join.svelte
+++ front/src/components/modal/join.svelte
@@ -6,11 +6,30 @@
   import Button from "../Button.svelte";
   import Typography from "../Typography.svelte";
   import type { ChannelExtended } from "$lib/models/prismaSchema";
+  import Slider from "../slider.svelte";
 
   const _Channel = new Channel();
+  const itemsPerPage = 10;
+  const levels = ["PROTECTED", "ALL", "PUBLIC"];
+  // none, ascending, descending
+  let sort: 0 | 1 | -1 = 0;
+  let sortString: "○" | "+" | "-" = "○";
   let search = "";
+  let selectedLevel = levels[1];
   let currentPage = 0;
-  const itemsPerPage = 2;
+
+  const setSort = () => {
+    if (sort == 0) {
+      sort = 1;
+      sortString = "+";
+    } else if (sort == 1) {
+      sort = -1;
+      sortString = "-";
+    } else {
+      sort = 0;
+      sortString = "○";
+    }
+  };
 
   const fetchChannel = async () => {
     await _Channel
@@ -23,21 +42,12 @@
     fetchChannel();
   });
   $: channels = $dataStore.channels;
+  // # Pagination
   $: totalPages = Math.ceil(channels.length / itemsPerPage);
   $: paginatedChannels = channels.slice(
     currentPage * itemsPerPage,
     (currentPage + 1) * itemsPerPage
   );
-  $: {
-    if (search.length > 3)
-      channels = $dataStore.channels.filter((chan) => {
-        const nameToLower = chan.name.toLowerCase();
-        const searchToLower = search.toLowerCase();
-
-        return nameToLower.includes(searchToLower);
-      });
-    else channels = $dataStore.channels;
-  }
 
   function nextPage() {
     if (currentPage < totalPages - 1) {
@@ -50,31 +60,85 @@
       currentPage--;
     }
   }
+  // # Filtering
+  // ## search and visibility
+  $: {
+    if (search || selectedLevel)
+      channels = $dataStore.channels.filter((chan) => {
+        const matchVisibility = (() => {
+          if (selectedLevel == "ALL") return true;
+          return chan.visibility == selectedLevel;
+        })();
+        const matchSearch = (() => {
+          if (!(search.length > 2)) return true;
+          const nameToLower = chan.name.toLowerCase();
+          const searchToLower = search.toLowerCase();
+          return nameToLower.includes(searchToLower);
+        })();
+
+        return matchSearch && matchVisibility;
+      });
+    else channels = $dataStore.channels;
+  }
+  // ## members length
+  $: {
+    switch (sort) {
+      case 0:
+        channels = channels.sort();
+        break;
+      case 1:
+        channels = channels.sort((a, b) => b.channelUsers.length - a.channelUsers.length);
+        break;
+      case -1:
+        channels = channels.sort((a, b) => a.channelUsers.length - b.channelUsers.length);
+        break;
+      default:
+        break;
+    }
+  }
 </script>
 
 <div class="joinchan">
-  <div class="top">
-    <Input
-      attributes={{
-        id: "search",
-        type: "text",
-        placeholder: "search",
-        name: "search",
-      }}
-      onChange={(value) => (search = value)}
-    />
-    <Button variant="error" disabled={search.length == 0}>
-      <Typography>{"x"}</Typography>
-    </Button>
+  <div class="filter">
+    <div class="slider-result">
+      <Slider
+        {levels}
+        initialValue={"ALL"}
+        onChange={(value) => {
+          selectedLevel = value;
+          console.log("slider change", value);
+        }}
+      />
+      <Typography class="... result">{`${paginatedChannels.length} / 10`}</Typography>
+    </div>
+    <div class="search">
+      <Input
+        attributes={{
+          id: "search",
+          type: "text",
+          placeholder: "search by name",
+          name: "search",
+        }}
+        onChange={(value) => (search = value)}
+      />
+      <Button variant="error" disabled={search.length == 0} on:click={() => (search = "")}>
+        <Typography>{"x"}</Typography>
+      </Button>
+      <Button on:click={async () => await fetchChannel()}>
+        <Typography>{"refresh"}</Typography>
+      </Button>
+    </div>
   </div>
   <div class="bot">
     <div class="content">
       <table>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Visibility</th>
-            <th>Members</th>
+            <th><Typography>{"Name"}</Typography></th>
+            <th><Typography>{"Visibility"}</Typography></th>
+            <th class="members" on:click={() => setSort()}
+              ><Typography>{`Members ${sortString}`}</Typography></th
+            >
           </tr>
         </thead>
         <tbody>
@@ -108,22 +172,36 @@
       >
       <Typography>{currentPage + 1} / {totalPages}</Typography>
       <Button on:click={nextPage} disabled={currentPage === totalPages - 1}>
-          <Typography>
+        <Typography>
           {"Previous"}
         </Typography>
-      
-      
       </Button>
     </div>
   </div>
 </div>
 
 <style lang="scss">
+  @import "../../lib/style/colors.scss";
+  :global(.slider-container) {
+    margin: 0 !important;
+  }
   .joinchan {
-    height: 70vh;
+    min-height: 50vh;
+    max-height: 70vh;
     width: 60vw;
+    display: flex;
+    flex-direction: column;
+    row-gap: 0.5em;
   }
   .top {
+    display: flex;
+  }
+  .slider-result {
+    column-gap: 0.5em;
+    display: flex;
+    justify-content: center;
+  }
+  .search {
     width: inherit;
     display: flex;
     flex-direction: row;
@@ -134,21 +212,64 @@
   }
   table {
     width: 100%;
-    border-collapse: collapse;
+    border-collapse: separate; // Changed to use border-radius on table cells
+    border-spacing: 0; // Removes spacing between cells
 
+    thead:after {
+      content: "";
+      display: block;
+      height: 0.3em; // Adjust this for desired "margin" height
+    }
+    .members {
+      cursor: pointer;
+    }
     th,
     td {
-      border: 1px solid #e1e1e1;
+      border: 1px solid black;
       padding: 0.5em;
       text-align: left;
     }
 
     th {
-      background-color: #f7f7f7;
+      background-color: darken($shipsOfficer, 8%);
+    }
+
+    tbody tr {
+      background-color: darken($shipsOfficer, 4%);
+      cursor: pointer;
     }
 
     tbody tr:hover {
-      background-color: #fafafa;
+      background-color: darken($shipsOfficer, 8%);
+      // background-color: #fafafa;
+    }
+
+    // Rounded top corners for the first row's first and last cell
+    tbody tr:first-child > td:first-child {
+      border-top-left-radius: 8px;
+    }
+
+    tbody tr:first-child > td:last-child {
+      border-top-right-radius: 8px;
+    }
+
+    // Rounded bottom corners for the last row's first and last cell
+    tbody tr:last-child > td:first-child {
+      border-bottom-left-radius: 8px;
+    }
+
+    tbody tr:last-child > td:last-child {
+      border-bottom-right-radius: 8px;
+    }
+
+    thead tr:first-child > th:first-child {
+      border-top-left-radius: 8px;
+      border-bottom-left-radius: 8px;
+    }
+
+    thead tr:last-child > th:last-child {
+      border-top-right-radius: 8px;
+      border-bottom-right-radius: 8px;
     }
   }
   .pagination {
@@ -156,5 +277,8 @@
     justify-content: space-between;
     align-items: center;
     margin-top: 1em;
+  }
+  :global(.result) {
+    align-self: center;
   }
 </style>
